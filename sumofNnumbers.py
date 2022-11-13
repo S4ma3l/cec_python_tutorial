c95771b02311923fa6b00c87faf3c2a8f9e29912
S4ma3l <66357485+S4ma3l@users.noreply.github.com>
2022-11-13 10:26:48 +0530

    Update sumofNnumbers.py

--- sumofNnumbers.py
+++ sumofNnumbers.py
@@ -1,6 +1,4 @@
-def sumOfNNumbers(num):
-    sum=0
-    for r in range(num+1):
-        sum+=r
+def sumOfNNumbers(num,sum):
+    for r in range(num+1):sum+=r
     return sum
-print(sumOfNNumbers(int(input("Enter a number")))*2)
+print(sumOfNNumbers(int(input("Enter a number")),0)*2)
